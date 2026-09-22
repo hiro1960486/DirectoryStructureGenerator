e8d791b88eef7c0882d43e71d11f5c5815218c79
hiro1960486 <86820392+hiro1960486@users.noreply.github.com>
2026-09-23 03:34:04 +0900

    Ver.2.8.0: Add safe quick preset updates

--- dsg_app/version.py
+++ dsg_app/version.py
@@ -1,11 +1,11 @@
 """Application version information.
 
-Version: 2.7.0
-Updated: 2026-09-22
+Version: 2.8.0
+Updated: 2026-09-23
 Author: hiro1960
 """
 
 APP_NAME = "Directory Structure Generator"
-APP_VERSION = "2.7.0"
-UPDATED = "2026-09-22"
+APP_VERSION = "2.8.0"
+UPDATED = "2026-09-23"
 AUTHOR = "hiro1960"
